2552c118954b38a89ed626e4c15f60340fac4a16
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-07-20 14:25:10 +0200

    Add hero

---
+++ src/components/hero.js
@@ -0,0 +1,198 @@
+import React from "react";
+
+export function Hero() {
+  return (
+    <section className="bg-shutter-black min-w-full">
+      <div className="text-white mx-auto container">
+        <h1 className="text-8xl pt-24 pb-20 leading-tight">
+          Preventing
+          <br />
+          frontrunning and
+          <br />
+          malicious MEV
+          <br />
+          on Ethereum
+        </h1>
+      </div>
+      <div className="absolute right-0 top-0 mt-28">
+        <svg
+          width="700"
+          height="714"
+          viewBox="0 0 700 714"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <mask
+            id="mask0"
+            mask-type="alpha"
+            maskUnits="userSpaceOnUse"
+            x="0"
+            y="0"
+            width="714"
+            height="714"
+          >
+            <circle cx="357" cy="357" r="357" fill="#C4C4C4" />
+          </mask>
+          <g mask="url(#mask0)">
+            <path
+              d="M0 700.794V-180.803"
+              stroke="white"
+              strokeWidth="0.274868"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M27.6368 700.794V-180.803"
+              stroke="white"
+              strokeWidth="1.03364"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M55.2083 700.794V-180.803"
+              stroke="white"
+              strokeWidth="1.79241"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M82.8451 700.794V-180.803"
+              stroke="white"
+              strokeWidth="2.55112"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M110.417 700.794V-180.803"
+              stroke="white"
+              strokeWidth="3.30989"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M138.053 700.794V-180.803"
+              stroke="white"
+              strokeWidth="4.06866"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M165.625 700.794V-180.803"
+              stroke="white"
+              strokeWidth="4.82737"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M193.261 700.794V-180.803"
+              stroke="white"
+              strokeWidth="5.58614"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M220.833 700.794V-180.803"
+              stroke="white"
+              strokeWidth="6.34491"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M248.47 700.794V-180.803"
+              stroke="white"
+              strokeWidth="7.10362"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M276.041 700.794V-180.803"
+              stroke="white"
+              strokeWidth="7.86239"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M303.678 700.794V-180.803"
+              stroke="white"
+              strokeWidth="8.62116"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M331.25 700.794V-180.803"
+              stroke="white"
+              strokeWidth="9.37987"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M358.886 700.794V-180.803"
+              stroke="white"
+              strokeWidth="10.1386"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M386.523 700.794V-180.803"
+              stroke="white"
+              strokeWidth="10.8974"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M414.095 700.794V-180.803"
+              stroke="white"
+              strokeWidth="11.6561"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M441.731 700.794V-180.803"
+              stroke="white"
+              strokeWidth="12.4149"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M469.303 700.794V-180.803"
+              stroke="white"
+              strokeWidth="13.1737"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M496.94 700.794V-180.803"
+              stroke="white"
+              strokeWidth="13.9324"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M524.511 700.794V-180.803"
+              stroke="white"
+              strokeWidth="14.6911"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M552.148 700.794V-180.803"
+              stroke="white"
+              strokeWidth="15.4499"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M579.719 700.794V-180.803"
+              stroke="white"
+              strokeWidth="16.2086"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M607.356 700.794V-180.803"
+              stroke="white"
+              strokeWidth="16.9674"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M634.928 700.794V-180.803"
+              stroke="white"
+              strokeWidth="17.7262"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M662.565 700.794V-180.803"
+              stroke="white"
+              strokeWidth="18.4849"
+              strokeMiterlimit="10"
+            />
+            <path
+              d="M690.136 700.794V-180.803"
+              stroke="white"
+              strokeWidth="19.2436"
+              strokeMiterlimit="10"
+            />
+          </g>
+        </svg>
+      </div>
+    </section>
+  );
+}
