d837a1dc8615278cba1807082d3c3aded2e2c6ec
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-07-21 15:45:02 +0200

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,4 @@
-const resolveConfig = require("tailwindcss/resolveConfig");
 const tailwindConfig = require("./tailwind.config.js");
-
-const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
@@ -15,13 +12,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
+        name: `Shutter Network`,
+        short_name: `Shutter`,
         start_url: `/`,
-        background_color: fullConfig.theme.colors.white,
-        theme_color: fullConfig.theme.colors.green["500"],
         display: `minimal-ui`,
-        icon: `src/images/tailwind-icon.png`,
+        icon: `src/images/favicon/favicon.svg`,
       },
     },
     {
