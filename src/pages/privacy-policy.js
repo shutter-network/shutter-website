dd48fb63177d4ab69ad768015415447836e2c805
nekhaly <nekhaly@gmail.com>
2021-08-12 14:57:51 +0200

    Add privacy policy page
    Reading from markdown file the content and enable link from footer.

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+export default function PrivacyPolicy(pageProps) {
+  return (
+    <Layout className="bg-shutter-black">
+      <SEO
+        keywords={["shutter", "network", "mev", "ethereum", "dkg", "privacy"]}
+        title="Privacy Policy"
+      />
+      <div className="bg-shutter-black text-white">
+        <section className="container mx-auto mb-20 px-4">
+        <div
+          className="privacy-policy mx-auto container flex flex-col space-y-4 py-10"
+          dangerouslySetInnerHTML={{ __html: pageProps.pageResources.data.markdownRemark.html }}
+        />
+        </section>
+    </div>
+  </Layout>
+  );
+}
+
+export const query = graphql`
+  query PrivacyPolicy {
+    markdownRemark(
+      fileAbsolutePath: { glob: "**/privacy-policy/privacy-policy.md" }
+    ) {
+      html
+    }
+  }
+`;
