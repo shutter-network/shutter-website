0c13d01ea6eb7f3c41d84ad2e772e073d97436d3
nekhaly <nekhaly@gmail.com>
2021-08-12 16:56:16 +0200

    Updates

--- component---cache-caches-gatsby-plugin-offline-app-shell-js-4f73cf8c10409d4da8af.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_tailwind=self.webpackChunkgatsby_starter_tailwind||[]).push([[306],{1930:function(t,e,n){"use strict";var a=n(5318);e.__esModule=!0,e.default=void 0;var u=a(n(5354)),r=a(n(7294)),l=function(t){function e(){return t.apply(this,arguments)||this}return(0,u.default)(e,t),e.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},e}(r.default.Component);e.default=l}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-4f73cf8c10409d4da8af.js.map
