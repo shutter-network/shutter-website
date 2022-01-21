2e7e18ef49175831436bdca4873d928c57736eb2
Smokyish <smoky@fury.fi>
2022-01-21 17:03:26 +0200

    Further SEO optimization

--- src/components/seo.js
+++ src/components/seo.js
@@ -24,56 +24,68 @@ function SEO({ lang, meta, keywords, title }) {
       }}
       meta={[
         {
-          property: `og:type`,
-          content: `website`,
+          name: `title`,
+          content: metaTitle,
         },
         {
-          property: `og:title`,
-          content: metaTitle + ` - Shutter Network`,
-        },
-        {
-          property: `og:image`,
-          content: PreviewImage,
-        },
-        {
-          property: `twitter:image`,
-          content: PreviewImage,
-        },
-        {
-          property: `title`,
-          content: `Shutter Network - Preventing front-running and malicious MEV on Ethereum`,
+          name: `description`,
+          content: `Shutter Network is a project to prevent front-running and malicious MEV on Ethereum by using threshold encryption`,
         },
         {
           name: `author`,
           content: `Shutter Network`,
         },
         {
-          property: `og:locale`,
-          content: `en_US`,
-        },
-        {
-          name: `description`,
-          content: `Shutter Network is an open-source project to prevent front-running on Ethereum by using a threshold cryptography-based distributed key generation protocol`,
-        },
-        {
-          property: `og:description`,
-          content: `Shutter Network is an open-source project to prevent front-running on Ethereum by using a threshold cryptography-based distributed key generation protocol`,
+          property: `og:type`,
+          content: `website`,
         },
         {
           property: `og:url`,
           content: `https://shutter.network/`,
         },
         {
+          property: `og:title`,
+          content: `Shutter Network - Home`,
+        },
+        {
+          property: `og:description`,
+          content: `Preventing front-running and malicious MEV on Ethereum`,
+        },
+        {
+          property: `og:locale`,
+          content: `en_US`,
+        },
+        {
           property: `og:site_name`,
           content: `Shutter Network`,
         },
         {
-          name: `twitter:card`,
-          content: `summary`,
+          property: `og:image`,
+          content: 'https://shutter.network' + PreviewImage,
         },
         {
-          property: `twitter:title`,
-          content: `Shutter Network - Preventing front-running and malicious MEV on Ethereum`,
+          property: `og:image:height`,
+          content: `630`,
+        },
+        {
+          property: `og:image:width`,
+          content: `1200`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`,
+        },
+        {
+          name: `twitter:title`,
+          content: `Shutter Network - Home`,
+        },
+        {
+          name: `twitter:description`,
+          content: `Preventing front-running and malicious MEV on Ethereum`,
+        },
+        {
+          property: `twitter:image`,
+          content: 'https://shutter.network' + PreviewImage,
         },
         {
           name: `twitter:site`,
@@ -82,6 +94,14 @@ function SEO({ lang, meta, keywords, title }) {
         {
           name: `twitter:creator`,
           content: `@project_shutter`,
+        },
+        {
+          name: `google-site-verification`,
+          content: `vniF73ij92Xco4_iHggmpNOrSP0bEbJfxonBETjemrQ`,
+        },
+        {
+          name: `msvalidate.01`,
+          content: `461226232A0CE4B34E124EE5F4510557`,
         },
       ]
         .concat(
