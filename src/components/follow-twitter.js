1dd3dd41b3933db9efc4e8034d2263d8f6b1059c
nekhaly <nekhaly@gmail.com>
2022-01-24 10:06:57 +0100

    Fix follow on twitter button in mobile size

--- src/components/follow-twitter.js
+++ src/components/follow-twitter.js
@@ -5,7 +5,7 @@ import gradesBg from "./../images/grades-background.svg";
 
 export function FollowTwitter() {
   return (
-    <section className="h-96 flex flex-col justify-center bg-shutter-black" style={{backgroundImage: `url(${gradesBg})`, backgroundSize: "contain"}}>
+    <section className="py-32 bg-shutter-black" style={{backgroundImage: `url(${gradesBg})`, backgroundSize: "contain"}}>
       <div className="md:container mx-auto px-8">
         <Button
           label="Follow us on twitter"
